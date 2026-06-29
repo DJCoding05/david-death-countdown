879ca36fe7e98d73900b39422be49e2d2dc62efa
dan <djpersonal158@gmail.com>
2026-06-28 21:05:30 -0500

    1.0

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,55 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 
 export default function Home() {
+  const daysTillTheDay = 14;
+  const [totalSeconds, setTotalSeconds] = useState<number>(daysTillTheDay*86400);
+  const [days,setDays] = useState(Math.floor(totalSeconds/(3600*24)));
+  const [hours, setHours] = useState(Math.floor((totalSeconds/3600)%24));
+  const [minutes, setMinutes] = useState(Math.floor((totalSeconds/60)%60));
+  const [seconds, setSeconds] = useState(totalSeconds%60);
+
+  const makeTimeVal = (timevar: number) => {
+    if(timevar < 10) {
+      return `0${timevar}`
+    } else {
+      return timevar
+    }
+  }
+
+  const handleChange = () => {
+    setDays(Math.floor(totalSeconds/(3600*24)))
+    setHours(Math.floor((totalSeconds/3600)%24))
+    setMinutes(Math.floor((totalSeconds/60)%60))
+    setSeconds(totalSeconds%60)
+    setTotalSeconds(totalSeconds-1);
+  }
+
+  const intervalId = setInterval(() => {
+    handleChange();
+  }, 1000);
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="flex font-sans flex-col gap-2 width-full min-h-screen justify-center items-center">
+      <h1 className="text-white text-xl">David's oil rig will explode in</h1>
+      <div className="flex flex-row gap-2">
+        <div className="flex flex-col gap-2">
+          <div className="border rounded-xl text-xl p-2">{makeTimeVal(days)}</div>
+          <div className="text-xl p-2">days</div>
+          </div>
+        <div className="flex flex-col gap-2">
+          <div className="border rounded-xl text-xl p-2">{makeTimeVal(hours)}</div>
+          <div className="text-xl p-2">hrs</div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="flex flex-col gap-2 justify-center">
+          <div className="border rounded-xl text-xl p-2">{makeTimeVal(minutes)}</div>
+          <div className="text-xl p-2">mins</div>
         </div>
-      </main>
+        <div className="flex flex-col gap-2">
+          <div className="border rounded-xl text-xl p-2">{makeTimeVal(seconds)}</div>
+          <div className="text-xl p-2">secs</div>
+        </div>
+      </div>
     </div>
   );
 }
